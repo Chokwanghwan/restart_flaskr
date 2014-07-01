d2949971c9924c6b1aa00e81cb7bb2ff17ca8de4
Chokwanghwan <kwang8992@gmail.com>
2014-07-01 21:18:34 +0900

    closed #1 1. close db 파라미터 추가 2. fetchall() 오타 수정  3. add_entry() 추가

--- flaskr.py
+++ flaskr.py
@@ -26,7 +26,7 @@ def get_db():
 		return g.sqlite_db
 
 @app.teardown_appcontext
-def close_db():
+def close_db(error):
 	if hasattr(g, 'sqlite_db'):
 		g.sqlite_db.close()
 
@@ -40,8 +40,33 @@ def init_db():
 def show_entries():
 	db = get_db()
 	cur = db.execute('select title, text from entries order by id desc')
-	entries = cur.fetchll()
+	entries = cur.fetchall()
 	return render_template('show_entries.html')		
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	db = get_db()
+	db.execute('insert into entries (title, text) value (?, ?)',
+		       [request.form['title'], request.form['text']])
+	db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
 	app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
