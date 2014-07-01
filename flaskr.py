2230ed0be02978f67ba9da745d8b9229a030f631
Chokwanghwan <kwang8992@gmail.com>
2014-07-01 22:18:00 +0900

    logout() 추

--- flaskr.py
+++ flaskr.py
@@ -67,6 +67,12 @@ def login():
 			flash('You were logged in')
 			return redirect(url_for('show_entries'))
 	return render_template('login.html', error = error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('you were logged out')
+	return redirect(url_for('show_entries'))
  
 if __name__ == '__main__':
 	app.run()
